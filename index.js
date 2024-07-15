99defdcec62de595a81664ddc6ce66e59a4f87e1
shahadat-siam <lonelinessismycompanion09@gmail.com>
2024-07-15 13:41:55 +0600

    delete function impelment

--- index.js
+++ index.js
@@ -1,22 +1,20 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config()
-const app = express()
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const port = process.env.PORT || 5000
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const app = express();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const port = process.env.PORT || 5000;
 
 //------ middlewere ----
 const corsOptions = {
-    origin:['http://localhost:5173', 'http://localhost:5174'],
-    credentials:true,
-    optionSuccessStatus:200,
-}
-app.use(cors(corsOptions))
-app.use(express.json())
- 
- 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ot34xl4.mongodb.net/?appName=Cluster0`;
+  origin: ["http://localhost:5173", "http://localhost:5174"],
+  credentials: true,
+  optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
+app.use(express.json());
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ot34xl4.mongodb.net/?appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -24,29 +22,37 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
+    const jobsCollection = client.db("FindJob").collection("jobs");
 
-    const jobsCollection = client.db('FindJob').collection('jobs')
-
-
-    // save job data in jobsCollection 
-    app.post('/jobs', async(req,res) => {
-        const jobData = req.body
-        const result = await jobsCollection.insertOne(jobData)
-         res.send(result)
-    })
+    // save job data in jobsCollection
+    app.post("/jobs", async (req, res) => {
+      const jobData = req.body;
+      const result = await jobsCollection.insertOne(jobData);
+      res.send(result);
+    });
 
     // get all jobs data from db
-    app.get('/all-jobs', async (req,res) => {     
-        const result = await jobsCollection.find().toArray()
-        res.send(result)
-    })
-     
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    app.get("/all-jobs", async (req, res) => {
+      const result = await jobsCollection.find().toArray();
+      res.send(result);
+    });
+
+    // delete a job data
+    app.delete("/jobs/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await jobsCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -54,13 +60,10 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-
-
-app.get('/', (req, res) => {
-    res.send('We are going to make an Find job website.')
-})
+app.get("/", (req, res) => {
+  res.send("We are going to make an Find job website.");
+});
 
 app.listen(port, () => {
-    console.log(`Find Job server running on port ${port}`)
-})
+  console.log(`Find Job server running on port ${port}`);
+});
